c9299e8db8de550458f075182868a2d861fd0ef3
Iulian <54375582+poenaruiulian@users.noreply.github.com>
2023-05-13 22:40:36 +0300

    Update AppMainNavigation.js

--- screens/AppScreens/AppMainNavigation.js
+++ screens/AppScreens/AppMainNavigation.js
@@ -5,25 +5,31 @@ import Docs from "./Docs";
 import Calendar from "./Calendar";
 import Settings from "./Settings";
 import {FontAwesome, MaterialIcons} from "@expo/vector-icons";
+import {BLUE} from "../../styles/ColorManager";
+import {Dimensions, StatusBar} from "react-native";
 
 const Tab = createBottomTabNavigator()
 
-export default function AppMainNavigation(){
+const SCREEN_HEIGHT = Dimensions.get('screen').height; // device height
+const STATUS_BAR_HEIGHT = StatusBar.currentHeight || 100;
+const WINDOW_HEIGHT = Dimensions.get('window').height;
+
+export default function AppNavigation(){
     return(
-        <Tab.Navigator tabBarOptions={{activeTintColor: "black", inactiveTintColor: 'white'}}>
+        <Tab.Navigator tabBarOptions={{activeTintColor: BLUE, inactiveTintColor: 'white'}}>
             <Tab.Screen  options={{headerShown:false, tabBarIcon: ({ focused }) => (
-                    <FontAwesome name="home" size={30} color={focused? "#123dff": "black"} /> ),
+                <FontAwesome name="home" size={30} color={focused? "#123dff": "black"} /> ),
             }}
-                         name={"Home"} component={Home}
+                        name={"Home"} component={Home}
             />
             <Tab.Screen options={{headerShown:false, tabBarIcon: ({ focused }) => (
-                    <MaterialIcons name="description" size={30} color={focused? "#123dff": "black"} /> )}}
+                    <MaterialIcons name="description" size={30} color={focused? BLUE: "black"} /> )}}
                         name={"Docs"} component={Docs}/>
             <Tab.Screen options={{headerShown:false, tabBarIcon: ({ focused }) => (
-                    <MaterialIcons name="event" size={30} color={focused? "#123dff": "black"} /> )}}
+                    <MaterialIcons name="event" size={30} color={focused? BLUE: "black"} /> )}}
                         name={"Calendar"} component={Calendar}/>
             <Tab.Screen options={{headerShown:false, tabBarIcon: ({ focused }) => (
-                    <MaterialIcons name="settings" size={30} color={focused? "#123dff": "black"} /> )}}
+                    <MaterialIcons name="settings" size={30} color={focused? BLUE: "black"} /> )}}
                         name={"Settings"} component={Settings} />
         </Tab.Navigator>
     )
